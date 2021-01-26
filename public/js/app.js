3917a05a339d2812f5a81ed454c8f8f90fd46238
Mark Rinker <mrrinker@gmail.com>
2021-01-25 21:55:53 -0500

    Removed unncessary code

--- public/js/app.js
+++ public/js/app.js
@@ -1,20 +1,3 @@
-console.log('Client side javascript file is loaded')
-
-// fetch('http://localhost:3001/weather?address=Fredericksburg').then((response)=>
-// {
-//     response.json().then((data) => 
-//     {
-//         if (!data.error)
-//         {
-//             console.log(data)
-//         }
-//         else
-//         {
-//             console.log(`Error: ${data.error}`)
-//         }
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const message1 = document.querySelector('#message-1')
