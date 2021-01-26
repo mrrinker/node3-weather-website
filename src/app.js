511e964e4cab365d6e39a31234c4fef94d6f3dae
Mark Rinker <mrrinker@gmail.com>
2021-01-26 17:47:21 -0500

    removed hard coded port

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -109,10 +110,7 @@ app.get('*', (req, res) =>
     })
 })
 
-const port = 3001
-
 app.listen(port, () => 
 {
     console.log(`Server is up on ${port}`)
 })
-
